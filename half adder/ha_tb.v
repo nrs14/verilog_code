974b08e67439f42a1bcbba607914ef534e548159
NAVEEN R.S <127390244+nrs14@users.noreply.github.com>
2023-08-03 15:41:23 +0530

    Update ha_tb.v

--- half adder/ha_tb.v
+++ half adder/ha_tb.v
@@ -1,34 +1,17 @@
 module ha_tb;
-
-	// Inputs
-	reg a;
-	reg b;
-
-	// Outputs
-	wire s;
-	wire co;
-
-	// Instantiate the Unit Under Test (UUT)
+	reg a,b; //input
+	wire s,co; //output
 	ha uut (
 		.a(a), 
 		.b(b), 
 		.s(s), 
 		.co(co)
-	);
-
-	initial begin
-		// Initialize Inputs
-		a = 0;
-		b = 0;
-
-		// Wait 100 ns for global reset to finish
+	);	// Instantiate the Unit Under Test (UUT)
+	initial begin	// Initialize Inputs
+		a = 0;b = 0;	// Wait 100 ns for global reset to finish
 		#100 a = 0;b = 1;
 		#100 a = 1;b = 0;
 		#100 a = 1;b = 1;
-        
-		// Add stimulus here
-
-	end
-      
+	end     
 endmodule
 
