8676f02583d6aa2a5ece7f2df475d3d98317affb
NAVEEN R.S <127390244+nrs14@users.noreply.github.com>
2023-08-12 10:00:29 +0530

    Update lfsr_tb.v

--- lfsr/lfsr_tb.v
+++ lfsr/lfsr_tb.v
@@ -1,29 +1,20 @@
 module lfsr1_tb;
-
-	// Inputs
+// Inputs
 	reg clk;
 	reg rst;
-
-	// Outputs
+// Outputs
 	wire [2:0] q;
-
 	// Instantiate the Unit Under Test (UUT)
 	lfsr1 uut (
 		.clk(clk), 
 		.rst(rst), 
 		.q(q)
 	);
-
 	always #100 clk = ~clk;  
-    // Testcase  
     initial begin  
         clk <= 0; rst=1;  
 		  #100 rst=0; 
 		  #100 clk <= 0; rst=1;  
 		  #100 rst=0; 		  
-	 
-
-
-	end
-      
+	 end   
 endmodule
